21d707688088fb0a03874b7221dd96725597159e
Matthieu <matthieu.dufour3@etu.univ-lorraine.fr>
2018-10-15 17:57:13 +0200

    WIP authentification

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,10 +11,50 @@ $app = new \Slim\App([
 	'settings' => [
 
 		'displayErrorDetaills' => true,
+	],
+
+	'db' => [
+	    'driver' => 'mysql',
+            'host' => 'localhost',
+            'database' => 'database',
+            'username' => 'root',
+            'password' => 'root',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
 	]
 ]);
 
 $container = $app->getContainer();
+
+$container['db'] = function (ContainerInterface $container) {
+    $settings = $container->get('database');
+    $capsule = new \Illuminate\Database\Capsule\Manager;
+    $capsule->addConnection($settings);
+    $capsule->setAsGlobal();
+    $capsule->bootEloquent();
+
+    return $capsule;
+};
+
+
+
+/* pas bon
+
+$capsule = new \Illuminate\Database\Capsule\Manager;
+$capsule->addConnection($container['settings']['db']);
+
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
+$container['db'] = function ($container) use ($capsule){
+	return $capsule;
+};
+
+*/
+
+
+
 
 $container['view'] = function ($container){
 	$view = new \Slim\Views\Twig('../resources/views', [
@@ -33,6 +73,9 @@ $container['HomeController'] = function ($container) {
 	return new \App\Controllers\HomeController($container);
 };
 
+$container['AuthController'] = function ($container) {
+	return new \App\Controllers\Auth\AuthController($container);
+};
 
 
 require '../app/routes.php';
